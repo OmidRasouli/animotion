0ecd96773b34d8702d23e0500b936bfbdb739f1c
OMiD <o.rasouli92@gmail.com>
2022-06-07 21:45:43 +0430

    Add viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import TimeLine from "./component/TimeLine/TimeLine";
+import style from "./App.module.scss";
 
 export default function App() {
   return (
     <div className={style.container}>
+      <div className={style.viewport}></div>
       <TimeLine />
     </div>
   );
 }
-
-export default App;
