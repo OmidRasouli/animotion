1be0e1e3bbe49b4aa4f826aba056f7fadac27164
OMiD <o.rasouli92@gmail.com>
2022-06-09 15:44:30 +0430

    Edit StyleBox

--- src/component/StyleBox/StyleBox.tsx
+++ src/component/StyleBox/StyleBox.tsx
@@ -1,1 +1,9 @@
-export default function StyleBox() {}
+import style from "./StyleBox.module.scss";
+
+export default function StyleBox() {
+  return (
+    <div className={style.box}>
+      <div className={style.title}>Style box</div>
+    </div>
+  );
+}
