54f02f189df2c364ae5ac52d0b77d11706192400
OMiD <o.rasouli92@gmail.com>
2022-06-09 14:41:39 +0430

    Add sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import TimeLine from "./component/TimeLine/TimeLine";
 import style from "./App.module.scss";
+import SideBar from "./component/SideBar/SideBar";
 
 export default function App() {
   return (
     <div className={style.container}>
-      <div className={style.viewport}></div>
+      <div className={style.viewport}>
+        <div className={style.result}>
+          <div className={style.output}></div>
+        </div>
+        <SideBar />
+      </div>
       <TimeLine />
     </div>
   );
