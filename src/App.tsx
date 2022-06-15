478867825badd7e40f158ec177ebc494a970191e
OMiD <o.rasouli92@gmail.com>
2022-06-15 21:21:50 +0430

    Add AnimationConfig to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import TimeLine from "./component/TimeLine/TimeLine";
 import style from "./App.module.scss";
 import SideBar from "./component/SideBar/SideBar";
+import AnimationConfig from "./component/AnimationConfig/AnimationConfig";
 
 export default function App() {
   return (
     <div className={style.container}>
       <div className={style.viewport}>
+        <AnimationConfig />
         <div className={style.result}>
           <div className={style.output}></div>
         </div>
