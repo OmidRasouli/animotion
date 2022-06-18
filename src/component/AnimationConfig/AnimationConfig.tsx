6ef9cc3091a15af2f3a12577402673e618ec5b86
OMiD <o.rasouli92@gmail.com>
2022-06-18 19:36:25 +0430

    Remove unused config

--- src/component/AnimationConfig/AnimationConfig.tsx
+++ src/component/AnimationConfig/AnimationConfig.tsx
@@ -23,18 +23,6 @@ export default function AnimationConfig({
   return (
     <div className={style["animation-config"]}>
       <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type={"text"}
-          id={"name"}
-          onChange={(e) =>
-            SetConfig({
-              config: { ...animationConfigState, name: e.target.value },
-            })
-          }
-        />
-      </div>
-      <div>
         <label htmlFor="duration">Duration:</label>
         <input
           type={"number"}
@@ -48,56 +36,6 @@ export default function AnimationConfig({
             })
           }
         />
-      </div>
-      <div>
-        <label htmlFor="delay">Delay:</label>
-        <input
-          type={"number"}
-          id={"delay"}
-          onChange={(e) =>
-            SetConfig({
-              config: {
-                ...animationConfigState,
-                delay: parseInt(e.target.value),
-              },
-            })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="iteration">Iteration:</label>
-        <input
-          type={"number"}
-          min={-1}
-          id={"iteration"}
-          onChange={(e) =>
-            SetConfig({
-              config: {
-                ...animationConfigState,
-                iteration: parseInt(e.target.value),
-              },
-            })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="direction">Direction:</label>
-        <select
-          id={"direction"}
-          onChange={(e) =>
-            SetConfig({
-              config: {
-                ...animationConfigState,
-                direction: e.target.value,
-              },
-            })
-          }
-        >
-          <option value={"normal"}>Normal</option>
-          <option value={"reverse"}>Reversse</option>
-          <option value={"alternate"}>Alternate</option>
-          <option value={"alternate-reverse"}>Alternate-Reverse</option>
-        </select>
       </div>
     </div>
   );
