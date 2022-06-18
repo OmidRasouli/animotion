b333be43f2dc05106e05628e6f012781b6d3d67f
OMiD <o.rasouli92@gmail.com>
2022-06-18 21:41:00 +0430

    Add variables to ruler

--- src/component/Ruler/Ruler.tsx
+++ src/component/Ruler/Ruler.tsx
@@ -1,4 +1,15 @@
-export default function Ruler({ orientation }: { orientation: string }) {
+export default function Ruler({
+  orientation,
+  width,
+  height,
+}: {
+  orientation: string;
+  width: number;
+  height: number;
+}) {
+  const bigIntervals: number = 100;
+  const smallIntervals: number = 5;
+
   return (
     <div>
       <canvas></canvas>
